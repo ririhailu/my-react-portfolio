211c3b7df8c38666856ab2e32a35f987744d78b7
Matrix <rhailu88@gmail.com>
2023-01-24 00:42:59 -0500

    created function for resume in components file.

---
+++ src/components/Resume/index.js
@@ -0,0 +1,41 @@
+import React from 'react'; 
+
+function Resume() {
+
+return(
+<section class="mb-5">
+    <h1 className="resume">resume: RahelHailu</h1>
+        <hr></hr>
+        <div class="row justify-content-center" id="resume">
+            <div class="mt-5 pl-5 pr-5">
+            I recently graduated from Penn LPS web development boot camp. In the class, I was able to create various web
+            applications. I also have a bachelor degree in Health administration and policy.
+                
+                
+                <p class="mt-5">
+                
+            
+                Download my full <a href=" https://github.com/ririhailu/my-react-portfolio/raw/master/src/assetscopy/Resume-Joseph-DeWoody.pdf" class="link">resume</a>
+                <br></br>  
+                </p>
+            </div>
+        </div>
+
+        <div class="justify-content-center mt-5">
+            <div id="front-back">
+                <h2>front end experience</h2>
+                <p>HTML, CSS (Bootstrap, Bulma), JavaScript, jQuery, React.js, Angular.js</p>
+            </div>
+    
+            <div id="front-back" class="mt-5">
+                <h2>back end experience</h2>
+                <p>
+                Node.js, Express.js, SQL (mySQ), Sequelize, NoSQL (MongoDB, Mongoose), API's (third-party, RESTful, server-side), Templating (Handlebars)
+                </p>
+            </div>
+        </div>
+</section>
+);
+}
+
+export default Resume;
