51f4a757591185f8ec40485cdbbfe4910b9cc4a2
Matrix <rhailu88@gmail.com>
2023-01-24 00:19:57 -0500

    Created import files and header with hashrouter in components file.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,3 +4,12 @@ function Footer() {
     return (
     <div class="footer" id="footer"></div>
     <a href="https://github.com/ririhailu"><img src="https://img.icons8.com/color/48/000000/github-2.png" alt="Github" class="icon"/></a>
+
+   ********* &copy; RahelHailu  |  <a href="https://github.com/ririhailu/my-react-portfolio/raw/master/src/assetscopy/******Resume-Joseph-DeWoody.pdf" class="link">download resume</a>  |  <a href="https://coolors.co/f06543-e8e9eb-e0dfd5-313638-f09d51">color palette</a>  |  made with <img id="react-icon" src="https://img.icons8.com/color/48/000000/react-native.png" alt="React"/>
+    </p>
+   
+</div>
+)
+}
+
+export default Footer;
