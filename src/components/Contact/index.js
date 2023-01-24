658dbd70268d2998a4e59c2ca3a67750e222f9f1
Matrix <rhailu88@gmail.com>
2023-01-23 23:30:23 -0500

    Added function for contact form in components file.

---
+++ src/components/Contact/index.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helper';
+
+function ContactForm() {
+
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const { name, email, message } = formState;
